e1ff37c5da966073000db60597958d581e44e9e9
Aleksei Zarubin <alex@zarubin.co>
2021-02-15 20:55:09 +0500

    test: QueryBuilder tests for storing, updating, and deleting a resource

--- tests/integration/builders/queryBuilder.test.ts
+++ tests/integration/builders/queryBuilder.test.ts
@@ -16,8 +16,8 @@ afterEach(() => {
 describe('QueryBuilder tests', () => {
 
 	test('retrieving a paginated list of resources', async () => {
-		server.schema.posts.create({title: "Test Post A"});
-		server.schema.posts.create({title: "Test Post B"});
+		server.schema.posts.create({title: 'Test Post A'});
+		server.schema.posts.create({title: 'Test Post B'});
 
 		const queryBuilder = new QueryBuilder<Post>(UrlBuilder.getResourceBaseUrl(Post), Post);
 
@@ -28,14 +28,49 @@ describe('QueryBuilder tests', () => {
 		expect(results.length).toBe(2);
 	});
 
+	test('storing a resource', async () => {
+		const queryBuilder = new QueryBuilder<Post>(UrlBuilder.getResourceBaseUrl(Post), Post);
+
+		const post = await queryBuilder.store({
+			title: 'Test Post'
+		});
+
+		expect(post).toStrictEqual<Post>(new Post({id: '1', title: 'Test Post'}));
+		expect(server.schema.posts.find('1').attrs.title).toBe('Test Post');
+	});
+
 	test('retrieving a resource', async () => {
-		server.schema.posts.create({title: "Test Post"});
+		server.schema.posts.create({title: 'Test Post'});
 
 		const queryBuilder = new QueryBuilder<Post>(UrlBuilder.getResourceBaseUrl(Post), Post);
 
 		const post = await queryBuilder.find('1');
 
 		expect(post).toStrictEqual<Post>(new Post({id: '1', title: 'Test Post'}));
+	});
+
+	test('updating a resource', async () => {
+		server.schema.posts.create({title: 'Test Post'});
+
+		const queryBuilder = new QueryBuilder<Post>(UrlBuilder.getResourceBaseUrl(Post), Post);
+
+		const post = await queryBuilder.update('1', {
+			title: 'Updated Post'
+		});
+
+		expect(post).toStrictEqual<Post>(new Post({id: '1', title: 'Updated Post'}));
+		expect(server.schema.posts.find('1').attrs.title).toBe('Updated Post');
+	});
+
+	test('deleting a resource', async () => {
+		server.schema.posts.create({title: 'Test Post'});
+
+		const queryBuilder = new QueryBuilder<Post>(UrlBuilder.getResourceBaseUrl(Post), Post);
+
+		const post = await queryBuilder.destroy('1');
+
+		expect(post).toStrictEqual<Post>(new Post({id: '1', title: 'Test Post'}));
+		expect(server.schema.posts.find('1')).toBeNull();
 	});
 
 });
--- tests/integration/mocks/server.ts
+++ tests/integration/mocks/server.ts
@@ -19,7 +19,28 @@ export default function makeServer() {
 			this.namespace = "api";
 
 			this.get("/posts");
+			this.post("/posts", function (schema: any, request) {
+				const attrs = JSON.parse(request.requestBody);
+
+				return schema.posts.create(attrs);
+			});
 			this.get("/posts/:id");
+			this.patch("/posts/:id",  (schema: any, request) => {
+				const id = request.params.id
+				const attrs = JSON.parse(request.requestBody);
+
+				let post = schema.posts.find(id);
+
+				return post.update(attrs)
+			});
+			this.del("/posts/:id", (schema: any, request) => {
+				const id = request.params.id;
+				const post = schema.posts.find(id);
+
+				post.destroy();
+
+				return post;
+			});
 		},
 	});
 }
