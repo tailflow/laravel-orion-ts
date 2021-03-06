aa9bb719f6fecad755f6411be6d80ea21130410c
Aleksei Zarubin <alex@zarubin.co>
2021-03-06 14:45:08 +0500

    test: Orion helper methods

--- tests/unit/orion.test.ts
+++ tests/unit/orion.test.ts
@@ -8,6 +8,31 @@ describe('Orion tests', () => {
 		expect(Orion.getApiUrl()).toBe('https://example.com/api/');
 	});
 
+	test('getting and setting token', () => {
+		Orion.setToken('test');
+
+		expect(Orion.getToken()).toBe('test');
+	});
+
+	test('unsetting token', () => {
+		Orion.setToken('test');
+		Orion.withoutToken();
+
+		expect(Orion.getToken()).toBeNull();
+	});
+
+	test('enabling withCredentials', () => {
+		Orion.withCredentials();
+
+		expect(Orion.usesCredentials()).toBeTruthy();
+	});
+
+	test('disabling withCredentials', () => {
+		Orion.withoutCredentials();
+
+		expect(Orion.usesCredentials()).toBeFalsy();
+	});
+
 	test('appending slash to the end when getting api url', () => {
 		Orion.setApiUrl('https://example.com/api');
 
