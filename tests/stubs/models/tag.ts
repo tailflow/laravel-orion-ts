90dcbae5a0ea841c2eacc7182b8cd6782a4c17ca
Aleksei Zarubin <alex@zarubin.co>
2021-03-06 14:45:00 +0500

    fix: model tests

--- tests/stubs/models/tag.ts
+++ tests/stubs/models/tag.ts
@@ -5,7 +5,7 @@ import BelongsToMany from "../../../src/drivers/default/relations/belongsToMany"
 export default class Tag extends Model<{
 	content: string,
 }> {
-	public posts(): BelongsToMany<Post> {
+	public posts() {
 		return new BelongsToMany(Post, this);
 	}
 }
--- tests/unit/model.test.ts
+++ tests/unit/model.test.ts
@@ -7,20 +7,20 @@ describe('Model tests', () => {
 
 	test('setting and getting key name', () => {
 		let model = new Post();
-		model.setKeyName('custom_key');
+		model.$setKeyName('custom_key');
 
-		expect(model.getKeyName()).toBe('custom_key');
+		expect(model.$getKeyName()).toBe('custom_key');
 	});
 
 	test('setting and getting key', () => {
 		let model = new Post();
-		model.setKey(123);
+		model.$setKey(123);
 
-		expect(model.getKey()).toBe(123);
+		expect(model.$getKey()).toBe(123);
 	});
 
 	test('getting resource name', () => {
-		expect(Post.prototype.getResourceName()).toBe('posts');
+		expect(Post.prototype.$getResourceName()).toBe('posts');
 	});
 
 });
