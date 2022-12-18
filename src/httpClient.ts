daa0fb015c75f6abf8335e65e93736d9ebda2ccd
Aleksei Zarubin <12220926+alexzarbn@users.noreply.github.com>
2022-12-18 21:24:53 +0100

    feat: rearrange http client method params

--- src/httpClient.ts
+++ src/httpClient.ts
@@ -15,8 +15,8 @@ export class HttpClient {
 	public async request<Response extends Record<string, unknown>>(
 		url: string,
 		method: HttpMethod,
-		params: Record<string, unknown> | null = {},
-		data: Record<string, unknown> = {}
+		params?: Record<string, unknown> | null,
+		data?: Record<string, unknown>
 	): Promise<AxiosResponse<Response>> {
 		const config: AxiosRequestConfig = Object.assign(Orion.getHttpClientConfig(), {
 			baseURL: this.baseUrl,
@@ -36,25 +36,25 @@ export class HttpClient {
 		return this.client.request<Response>(config);
 	}
 
-	public async get<Response extends Record<string, unknown>>(url: string, params: Record<string, unknown>): Promise<AxiosResponse<Response>> {
+	public async get<Response extends Record<string, unknown>>(url: string, params?: Record<string, unknown>): Promise<AxiosResponse<Response>> {
 		return this.request<Response>(
 			url, HttpMethod.GET, params
 		)
 	}
 
-	public async post<Response extends Record<string, unknown>>(url: string, params: Record<string, unknown>, data: Record<string, unknown>): Promise<AxiosResponse<Response>> {
+	public async post<Response extends Record<string, unknown>>(url: string, data: Record<string, unknown>, params?: Record<string, unknown>,): Promise<AxiosResponse<Response>> {
 		return this.request<Response>(
 			url, HttpMethod.POST, params, data
 		)
 	}
 
-	public async patch<Response extends Record<string, unknown>>(url: string, params: Record<string, unknown>, data: Record<string, unknown>): Promise<AxiosResponse<Response>> {
+	public async patch<Response extends Record<string, unknown>>(url: string, data: Record<string, unknown>, params?: Record<string, unknown>, ): Promise<AxiosResponse<Response>> {
 		return this.request<Response>(
 			url, HttpMethod.PATCH, params, data
 		)
 	}
 
-	public async delete<Response extends Record<string, unknown>>(url: string, params: Record<string, unknown>): Promise<AxiosResponse<Response>> {
+	public async delete<Response extends Record<string, unknown>>(url: string, params?: Record<string, unknown>): Promise<AxiosResponse<Response>> {
 		return this.request<Response>(
 			url, HttpMethod.DELETE, params
 		)
